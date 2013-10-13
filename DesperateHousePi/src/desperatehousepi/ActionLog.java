12e20c567e643eaf8e2a04701774d2044703b818
bjbraune <bjbraune@ucsc.edu>
2013-10-12 18:39:09 -0700

    Update and rename ActionLog.java to DesperateHousePi/src/desperatehousepi/ActionLog.java

--- DesperateHousePi/src/desperatehousepi/ActionLog.java
+++ DesperateHousePi/src/desperatehousepi/ActionLog.java
@@ -1,8 +1,17 @@
-import java.io.*;
-import java.lang.*;
+package desperatehousepi;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ActionLog{
-
+   
+   
+   /*********************************
+    * Takes in an action string as an argument 
+    * and appends it to the file "log.txt".
+    * @author Brad
+    * *******************************/
    public static void logAction(String action) throws IOException{
       try{
          FileWriter fstream = new FileWriter("log.txt", true);
