2b70e21de3b9071ecee82ed823fbeb1f02885151
bjbraune <bjbraune@ucsc.edu>
2013-12-05 01:45:36 -0800

    Create TestLog.java

---
+++ DesperateHousePi/desperatehousepi/Tests/TestLog.java
@@ -0,0 +1,40 @@
+package desperatehousepi.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import desperatehousepi.Crust.Crust;
+import desperatehousepi.Crust.ActionLog;
+
+public class TestLog {
+
+        
+        @Test
+        public void testActionLog() throws FileNotFoundException{
+        
+              Crust c = new Crust();
+              assertNotNull(c);
+        
+              ActionLog hist = new ActionLog(c);
+              asertNotNull(hist);
+              
+              hist.logAction("this is an action");
+              
+              Scanner sc = new Scanner(new File("John_Jacob_Smith_log.txt"));
+		          while(sc.hasNextLine()){
+		                String str = sc.nextLine();
+		                System.out.println(str);
+		          }
+		          sc.close();
+        }
+}
+        
+        
+        
+        
+        
