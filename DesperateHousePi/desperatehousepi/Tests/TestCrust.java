db5112d867bb48425dd538cf8ea2c92d32f2c650
hho1 <honloongho@Dolph.local>
2013-12-04 22:38:52 -0800

    JUnit test for goodDate module inside CreationWindow.java

--- DesperateHousePi/desperatehousepi/Tests/TestCrust.java
+++ DesperateHousePi/desperatehousepi/Tests/TestCrust.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import desperatehousepi.Crust.Crust;
+import desperatehousepi.GUI.CreationWindow;
 
 public class TestCrust {
 	
@@ -99,12 +100,34 @@ public class TestCrust {
 		
 	}
 	
+	
+	/*
+	 * Testing goodDate module inside CreationWindow.java
+	 * Input:String str_month,String str_day
+	 * Output: boolean
+	 * Equivalence classes: 1. month not an Integer sent as String
+	 * 						2. day not an Integer sent as String
+	 * 						3. month is Feb
+	 * 						4. 31 days months (1,3,5,7,8,10,12)
+	 * 						5. 30 days months (4,6,9,11)
+	 * 						6. month < 1 or month > 12
+	 * 						7. 0 < day < 30
+	 * 						8. day = 30
+	 * 						9. day = 31
+	 * 						10. day < 1 or day > 31
+	 */
 	@Test
-	public void testTime(){
-		Crust tCrust = new Crust("John","Jacob","Smith",10,20);
-		assertNotNull(tCrust);
-		assertTrue(tCrust.getbdaymonth()==10);
-		assertTrue(tCrust.getbirthday()==20);
+	public void testgoodDate(){
+		CreationWindow twindow = new CreationWindow();
+		assertFalse(twindow.goodDate("asd", "20"));// EC 1,7
+		assertFalse(twindow.goodDate("2", "qwe")); // EC 2,3
+		assertFalse(twindow.goodDate("14", "20")); // EC 6,7
+		assertFalse(twindow.goodDate("9", "400")); // EC 5,10
+		assertFalse(twindow.goodDate("2", "30"));  // EC 3,8
+		assertFalse(twindow.goodDate("6", "31"));  // EC 5,9
+		assertTrue(twindow.goodDate("1", "31"));   // EC 4,9
+		assertTrue(twindow.goodDate("4", "30"));   // EC 5,8
+		assertTrue(twindow.goodDate("2", "29"));   // EC 3,7
 	}
 	
 }
